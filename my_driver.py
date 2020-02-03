6f014fc63421fceb978d6e9e3cb254e45ee70e99
numan98khan <numan98khan@gmail.com>
2020-02-03 22:43:48 +0500

    Update my_driver.py
    completed code for demo

--- my_driver.py
+++ my_driver.py
@@ -1,10 +1,32 @@
 from pytocl.driver import Driver
 from pytocl.car import State, Command
 import pickle
-
+import base
 
 class MyDriver(Driver):
+    # Override the `drive` method to create your own driver
     ...
     def drive(self, carstate: State) -> Command:
-        
-        # return command
+        # Interesting stuff
+        # print(carstate)
+
+        sensor = base.MySensorModel()
+        sensor.setCarState(carstate)
+
+
+        driver = base.HeuristicDriver()
+        recievedAct = driver.control(sensor)
+
+        # print(">>>Decided Move: ", recievedAct.toString())
+
+        # print(">>>For State: \n", carstate)
+
+        command = Command()
+        command.accelerator = recievedAct.accelerate
+        command.brake = recievedAct.brake
+        command.gear = recievedAct.gear
+        command.steering = recievedAct.steering
+        command.focus = recievedAct.focus
+        command.clutch = recievedAct.clutch
+
+        return command
